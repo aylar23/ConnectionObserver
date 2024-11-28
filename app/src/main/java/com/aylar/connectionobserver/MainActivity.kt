506e1733960cd39990539e5f5eaa8a5456963968
aylar23 <aylarbayram23@gmail.com>
2024-11-28 13:30:26 +0500

    Add Connectivity Viewmodel and observe connection

--- app/src/main/java/com/aylar/connectionobserver/MainActivity.kt
+++ app/src/main/java/com/aylar/connectionobserver/MainActivity.kt
@@ -4,13 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.aylar.connectionobserver.ui.theme.ConnectionObserverTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +24,27 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ConnectionObserverTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                val viewModel = viewModel<ConnectivityViewModel> {
+                    ConnectivityViewModel(
+                        connectivityObserver = AndroidConnectivityObserver(
+                            context = applicationContext
+                        )
                     )
+                }
+                val isConnected by viewModel.isConnected.collectAsStateWithLifecycle()
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text(
+                            text = "Connected? $isConnected"
+                        )
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ConnectionObserverTheme {
-        Greeting("Android")
     }
 }
